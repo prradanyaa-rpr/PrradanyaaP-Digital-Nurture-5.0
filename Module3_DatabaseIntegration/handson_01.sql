feb44152e09474b2d28a3bb401cfcc3c8c69ee5c
prradanyaa-rpr <prradanyaa@gmail.com>
2026-06-24 19:44:07 +0530

    Update handson_01.sql

--- Module3_DatabaseIntegration/handson_01.sql
+++ Module3_DatabaseIntegration/handson_01.sql
@@ -1,1 +1,50 @@
 
+CREATE DATABASE college_db;
+USE college_db;
+
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY AUTO_INCREMENT,
+    dept_name VARCHAR(100) NOT NULL,
+    hod_name VARCHAR(100),
+    budget DECIMAL(12,2)
+);
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    date_of_birth DATE,
+    department_id INT,
+    enrollment_year INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY AUTO_INCREMENT,
+    course_name VARCHAR(150) NOT NULL,
+    course_code VARCHAR(20) NOT NULL UNIQUE,
+    credits INT,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+CREATE TABLE professors (
+    professor_id INT PRIMARY KEY AUTO_INCREMENT,
+    prof_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    department_id INT,
+    salary DECIMAL(10,2),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+CREATE TABLE enrollments (
+    enrollment_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT,
+    course_id INT,
+    enrollment_date DATE,
+    grade CHAR(2), -- Nullable by default
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
