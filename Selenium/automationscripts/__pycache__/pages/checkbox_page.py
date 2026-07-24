4efb67c8599e1d6f8d7d87dceb08046b229687f6
prradanyaa-rpr <prradanyaa@gmail.com>
2026-07-24 20:26:37 +0530

    Update checkbox_page.py

--- Selenium/automationscripts/__pycache__/pages/checkbox_page.py
+++ Selenium/automationscripts/__pycache__/pages/checkbox_page.py
@@ -1,1 +1,31 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
+
+class CheckboxPage(BasePage):
+
+    def get_checkbox(self, index):
+        return (
+            By.XPATH,
+            f"(//input[@type='checkbox'])[{index}]"
+        )
+
+    def check_option(self, index):
+        checkbox = self.wait_for_element(
+            self.get_checkbox(index)
+        )
+        if not checkbox.is_selected():
+            checkbox.click()
+
+    def uncheck_option(self, index):
+        checkbox = self.wait_for_element(
+            self.get_checkbox(index)
+        )
+        if checkbox.is_selected():
+            checkbox.click()
+
+    def is_option_checked(self, index):
+        checkbox = self.wait_for_element(
+            self.get_checkbox(index)
+        )
+        return checkbox.is_selected()
